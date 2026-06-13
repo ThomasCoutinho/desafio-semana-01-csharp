82b955ea933f720d7ddd24c64516ac925520584d
Thomas Coutinho <thomas.coutinho@hotmail.com>
2026-06-13 16:07:37 -0300

    Exercício 1 finalizado

--- Exercicio1ApresentacaoPessoal/Program.cs
+++ Exercicio1ApresentacaoPessoal/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿var nome = "Thomas Coutinho";
+int idade = 33;
+var cidadeNatal = "Curitiba";
+var profSonhos = "Desenvolvedor Backend";
+
+Console.WriteLine("============================================");
+Console.WriteLine("           CARTÃO DE APRESENTAÇÃO           ");
+Console.WriteLine("============================================");
+Console.WriteLine($"Nome: {nome}");
+Console.WriteLine($"Idade: {idade}");
+Console.WriteLine($"Cidade: {cidadeNatal}");
+Console.WriteLine($"Profissão dos sonhos: {profSonhos}");
+Console.WriteLine("============================================");
