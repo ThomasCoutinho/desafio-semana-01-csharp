bc67f843087c1d0d787ae579e339e153e33081a9
Thomas Coutinho <thomas.coutinho@hotmail.com>
2026-06-13 22:13:53 -0300

    Exercício 4 finalizado

--- Exercicio4VerificadorDeIdade/Program.cs
+++ Exercicio4VerificadorDeIdade/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("============================================");
+Console.WriteLine("            VERIFICADOR DE IDADE            ");
+Console.WriteLine("============================================");
+
+Console.Write("Digite seu ano de nascimento: ");
+int anoNascimento = int.Parse(Console.ReadLine());
+
+int idade = DateTime.Now.Year - anoNascimento;
+
+Console.WriteLine("\n============================================");
+Console.WriteLine($"Você tem {idade} anos.");
+
+string verificaIdade = idade >= 18 ? "maior de idade!" : "menor de idade!";
+
+Console.WriteLine($"Portanto, é {verificaIdade}");
+Console.WriteLine("============================================");
